f4de55db977897675ebbc8b1e34d735a72bc71b2
tochibedford <musicofbyte@gmail.com>
2023-02-26 14:59:49 +0100

    registered scroll trigger

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.scss'
-import gsap from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import ThemeContext from './context/Theme';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
