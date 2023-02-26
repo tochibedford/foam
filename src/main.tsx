dc7396aff79a3fee5839be93050b388a7ad22eee
tochibedford <musicofbyte@gmail.com>
2023-02-26 23:24:19 +0100

    css resets

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.scss'
+import './font/stylesheet.scss'
 import ThemeContext from './context/Theme';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
