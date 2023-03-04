7a091f251c74641e3fbaab54228d64c991d94f8a
tochibedford <musicofbyte@gmail.com>
2023-03-04 08:34:40 +0100

    moved ThemeProvider down the tree

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,19 @@ import { useEffect, useRef } from 'react'
 import styles from './App.module.scss'
 import Loader from './components/Loader';
 import NowOnView from './components/NowOnView';
+import ThemeContext from './context/Theme';
 
 function App() {
 
   return (
-    <div className={styles.App}>
-      <Loader />
-      <main className={styles.mainContent}>
-        <NowOnView />
-      </main>
-    </div>
+    <ThemeContext.Provider value={0}>
+      <div className={styles.App}>
+        <Loader />
+        <main className={styles.mainContent}>
+          <NowOnView />
+        </main>
+      </div>
+    </ThemeContext.Provider>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.scss'
 import './font/stylesheet.scss'
-import ThemeContext from './context/Theme';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ThemeContext.Provider value={0}>
-      <App />
-    </ThemeContext.Provider>
+    <App />
   </React.StrictMode>,
 )
