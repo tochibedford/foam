e0a1812b4b15832ef9a8138864c3acf1e65264c6
tochibedford <musicofbyte@gmail.com>
2023-03-04 08:34:20 +0100

    implemented basic NowInView Component

--- src/components/NowOnView/index.tsx
+++ src/components/NowOnView/index.tsx
@@ -1,8 +1,27 @@
 import styles from "./NowOnView.module.scss"
+import bannerImage from "./assets/bannerImage.webp"
 const NowOnView = () => {
 
     return (
-        <div>Now</div>
+        <div className={styles.onViewContainer}>
+            <a className={styles.banner}>
+                <div className={styles.bannerImageContainer}>
+                    <img src={bannerImage} alt="Now In View Image" />
+                </div>
+                <div className={styles.bannerText}>
+                    <span className={styles.exhibitionInfo}>
+                        <div className={styles.tag}>exhibition</div>
+                        <div className={styles.date}>until 14 Jun</div>
+                    </span>
+                    <div className={styles.artist}>
+                        Yulia Matvienko
+                    </div>
+                    <div className={styles.title}>
+                        HAVE SOME CAT
+                    </div>
+                </div>
+            </a>
+        </div>
     )
 }
 
